25a18a7666411d4463ba8ded001562b300367368
Ronak Panchal <ronakpnchl@gmail.com>
2018-01-26 17:54:22 -0500

    updated

--- src/com/leetcode/problems/MaxConsecutiveOnesII.java
+++ src/com/leetcode/problems/MaxConsecutiveOnesII.java
@@ -8,9 +8,11 @@ public class MaxConsecutiveOnesII {
 		int lastZero = -1;
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] == 0) {
-				if (lastZero == -1) {lastZero = i;curr++;}
-				else {curr = i - lastZero;lastZero = i;}
-			} else curr++;
+				lastZero = i;
+				if (lastZero == -1) curr++;
+				else curr = i - lastZero;
+			} else 
+				curr++;
 			max = Math.max(max, curr);
 		}
 		return max;
