442244c70e6e5f95033e7437a55f234932476687
Ronak Panchal <ronakpnchl@gmail.com>
2018-01-26 17:44:11 -0500

    updates and restructured

--- src/com/leetcode/problems/MajorityElement.java
+++ src/com/leetcode/problems/MajorityElement.java
@@ -1,21 +1,13 @@
 package com.leetcode.problems;
 
 public class MajorityElement {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	public int majorityElement(int[] nums) {
         int candidate = nums[0];
         int count = 1;
         int n = nums.length;
         for (int i = 1;i < nums.length;i++) {
-            if (count == 0) {
-                candidate = nums[i];
-                count = 1;
-            } 
+            if (count == 0) {candidate = nums[i];count = 1;} 
             else if (nums[i] == candidate) count++;
             else if (nums[i] != candidate) count--;
         }
--- src/com/leetcode/problems/MajorityElementII.java
+++ src/com/leetcode/problems/MajorityElementII.java
@@ -1,44 +1,30 @@
 package com.leetcode.problems;
 
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 public class MajorityElementII {
 	
-	public List<Integer> majorityElement(int[] nums) {
-        if (nums.length == 0) return Collections.emptyList();
-        List<Integer> result = new ArrayList<Integer>();
-        int candidate1 = nums[0];
-        int candidate2 = nums[0];
-        int count1 = 0;
-        int count2 = 0;
-        for (int i = 0;i < nums.length;i++) {
-            if (nums[i] == candidate1) count1++;
-            else if (nums[i] == candidate2)count2++;
-            else if (count1 == 0) {
-                count1 = 1;
-                candidate1 = nums[i];
-            } else if (count2 == 0) {
-                count2 = 1;
-                candidate2 = nums[i];
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-        count1 = 0;
-        count2 = 0;
-        for (int i = 0;i < nums.length;i++) {
-            if (nums[i] == candidate1) {
-                count1++;
-            } else if (nums[i] == candidate2) {
-                count2++;
-            }
-        }
-        if (count1 > (nums.length / 3)) result.add(candidate1);
-        if (count2 > (nums.length/ 3)) result.add(candidate2);
-        return result;
-    }
+	 public List<Integer> majorityElement(int[] nums) {
+	        List<Integer> result = new ArrayList<Integer>();
+	        int c1 = 0, c2 = 0, count1 = 0, count2 = 0;
+	        for (int i = 0;i < nums.length;i++) {
+	            if (nums[i] == c1) count1++;
+	            else if (nums[i] == c2)count2++;
+	            else if (count1 == 0) {count1 = 1;c1 = nums[i];}
+	            else if (count2 == 0) {count2 = 1;c2 = nums[i];}
+	            else {count1--;count2--;}
+	        }
+	        count1 = 0;
+	        count2 = 0;
+	        for (int i = 0;i < nums.length;i++) {
+	            if (nums[i] == c1) count1++;
+	            if (nums[i] == c2) count2++;
+	        }
+	        if (count1 > (nums.length / 3)) result.add(c1);
+	        if (c1 == c2) return result;
+	        if (count2 > (nums.length / 3)) result.add(c2);
+	        return result;
+	    }
 
 }
