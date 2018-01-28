aa6e0001a486ad7c25783af8de3684e8e91d4eeb
Ronak Panchal <ronakpanchal@users.noreply.github.com>
2018-01-28 04:24:25 -0500

    Update BattleshipsInBoard.java

--- src/com/leetcode/problems/BattleshipsInBoard.java
+++ src/com/leetcode/problems/BattleshipsInBoard.java
@@ -20,8 +20,7 @@ public class BattleshipsInBoard {
 	}
 
 	void countBattleships(char[][] board, int i, int j, char dir) {
-		if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'X')
-			return;
+		if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'X') return;
 		if (dir == 'V') {
 			board[i][j] = '.';
 			countBattleships(board, i + 1, j, 'V');
