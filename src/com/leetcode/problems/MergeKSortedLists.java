b787dcaf3b2d5ccf581a8c07baaf94047133e164
Ronak Panchal <ronakpanchal@users.noreply.github.com>
2018-02-04 14:42:13 -0500

    Update MergeKSortedLists.java

--- src/com/leetcode/problems/MergeKSortedLists.java
+++ src/com/leetcode/problems/MergeKSortedLists.java
@@ -7,25 +7,25 @@ import java.util.Queue;
 public class MergeKSortedLists {
 	
 	public ListNode mergeKLists(ListNode[] lists) {
-        Queue<ListNode> pq = new PriorityQueue<ListNode>(new Comparator<ListNode>(){
-            public int compare(ListNode a, ListNode b) {
-                return a.val - b.val;
-            }
-        });
-        for (ListNode node:lists) {
-            if (node != null) pq.add(node);
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode p = dummy;
-        while (!pq.isEmpty()) {
-            ListNode node = pq.poll();
-            p.next = node;
-            if (node.next != null) {
-                pq.add(node.next);
-            }
-            p = p.next;
-        }
-        return dummy.next;
-    }
+		Queue<ListNode> pq = new PriorityQueue<ListNode>(new Comparator<ListNode>(){
+		    public int compare(ListNode a, ListNode b) {
+			return a.val - b.val;
+		    }
+		});
+		for (ListNode node:lists) {
+		    if (node != null) pq.add(node);
+		}
+		ListNode dummy = new ListNode(0);
+		ListNode p = dummy;
+		while (!pq.isEmpty()) {
+		    ListNode node = pq.poll();
+		    p.next = node;
+		    if (node.next != null) {
+			pq.add(node.next);
+		    }
+		    p = p.next;
+		}
+		return dummy.next;
+    	}
 
 }
