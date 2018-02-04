616db4ad74b82a3edd201481d2fc57926472a025
Ronak Panchal <ronakpanchal@users.noreply.github.com>
2018-02-04 14:38:42 -0500

    Update ContinuousSubarraySum.java

--- src/com/leetcode/problems/ContinuousSubarraySum.java
+++ src/com/leetcode/problems/ContinuousSubarraySum.java
@@ -6,19 +6,19 @@ import java.util.Map;
 public class ContinuousSubarraySum {
 	
 	public boolean checkSubarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(0, -1);
-        int runningSum = 0;
-        for (int i = 0;i < nums.length;i++) {
-            runningSum += nums[i];
-            if (k != 0) runningSum = runningSum % k;
-            if (map.containsKey(runningSum)) {
-                if (i - map.get(runningSum) > 1) return true;
-            } else {
-                map.put(runningSum ,i);
-            } 
-        }
-        return false;
-    }
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(0, -1);
+		int runningSum = 0;
+		for (int i = 0;i < nums.length;i++) {
+		    runningSum += nums[i];
+		    if (k != 0) runningSum = runningSum % k;
+		    if (map.containsKey(runningSum)) {
+			if (i - map.get(runningSum) > 1) return true;
+		    } else {
+			map.put(runningSum ,i);
+		    } 
+		}
+		return false;
+    	}
 
 }
